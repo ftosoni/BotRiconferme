ad9b2c1a4dd94cf5cf7e3375eab5d8b5b2d3fb0d
Francesco Tosoni <francitosoni@gmail.com>
2026-09-21 15:53:35 +0200

    add copyright/licence header, debug CI/CD tests, add Code of conduct

--- src/Wiki/Page/PageRiconferma.php
+++ src/Wiki/Page/PageRiconferma.php
@@ -1,4 +1,25 @@
-<?php declare( strict_types=1 );
+<?php
+/**
+ * This file is part of BotRiconferme.
+ *
+ * BotRiconferme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BotRiconferme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (C) 2019-2026 Daimona Eaytoy
+ * @license AGPL-3.0-or-later
+ */
+
+declare( strict_types=1 );
 
 namespace BotRiconferme\Wiki\Page;
 
@@ -46,6 +67,7 @@ class PageRiconferma extends Page {
 		if ( !isset( $this->opposeSection ) ) {
 			throw new RuntimeException( "Cannot find oppose section!" );
 		}
+		// @phan-suppress-next-line PhanRedundantCondition Lazy-loaded typed property may be uninitialized
 		if ( !isset( $this->supportSection ) && $this->isVote() ) {
 			throw new RuntimeException( "Cannot find support section for vote!" );
 		}
